2fa766d2fe3906a9784f175b6bd0b3fb108e99cc
Shriprasanna <shriprasanna32@gmail.com>
2023-04-18 23:39:14 +0530

    final loading figeredin out

---
+++ app/data/[name]/loading.tsx
@@ -0,0 +1,7 @@
+const loading = () => {
+  return (
+    <div className="w-[200px] h-[100px] rounded bg-white/20 animate-pulse border-2 border-sky-700 p-2"></div>
+  );
+};
+
+export default loading;
--- app/data/[name]/page.tsx
+++ app/data/[name]/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import data from "../../api/hello/data.json";
 import Link from "next/link";
 
@@ -12,24 +12,25 @@ export async function generateMetadata({ params }: paramsType) {
   };
 }
 
-const dynamicRouteName = ({ params }: paramsType) => {
-  const filterName: any = data.filter((v) => v.name === params.name);
+async function check() {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  return data.filter((v) => v.name === "cs");
+}
+
+const dynamicRouteName = async ({ params }: paramsType) => {
+  let filterName: any = await check();
 
   return (
     <>
-      <Suspense
-        fallback={<h1 className="text-sky-500">Loading ............</h1>}
-      >
-        <div className="text-white flex flex-col p-5 border border-sky-500 w-[350px]">
-          {params.name}
-          <h1>name of the game : {filterName[0]?.name} </h1>
-          <h1> rating out of 10 : {filterName[0]?.rating}</h1>
-        </div>
+      <div className="text-white flex flex-col p-5 border border-sky-500 w-[350px]">
+        {params.name}
+        <h1>name of the game : {filterName[0]?.name} </h1>
+        <h1> rating out of 10 : {filterName[0]?.rating}</h1>
+      </div>
 
-        <Link href="/data" className="bg-sky-500 p-2">
-          <button>Back to all games</button>
-        </Link>
-      </Suspense>
+      <Link href="/data" className="bg-sky-500 p-2">
+        <button>Back to all games</button>
+      </Link>
     </>
   );
 };
