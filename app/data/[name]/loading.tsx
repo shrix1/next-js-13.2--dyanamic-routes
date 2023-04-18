7dfbe274280dc0d5f52d59f62f91389c74e07195
Shriprasanna <shriprasanna32@gmail.com>
2023-04-19 00:12:32 +0530

    commit this branch chnages before pulling code from main branch

--- app/data/[name]/loading.tsx
+++ app/data/[name]/loading.tsx
@@ -1,6 +1,8 @@
 const loading = () => {
   return (
-    <div className="w-[200px] h-[40px] rounded bg-white/20 animate-pulse p-5"></div>
+    <div className="w-[200px] h-[40px] rounded bg-white/20 animate-pulse p-5">
+      sasas
+    </div>
   );
 };
 
