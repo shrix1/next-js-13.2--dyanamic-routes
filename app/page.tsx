c78b434295f650cfaf8466a3f473d3425c3fc544
Shriprasanna <shriprasanna32@gmail.com>
2023-04-22 21:05:56 +0530

    learning framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,86 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 import Link from "next/link";
-import { Suspense } from "react";
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+
   return (
-    <Suspense fallback={<h1 className="text-sky-500">loading</h1>}>
-      <main>
-        <h1 className="text-2xl text-sky-500 text-center">
-          <Link href="/data">dynamicRout</Link>
-        </h1>
-      </main>
-    </Suspense>
+    <main>
+      <h1 className="text-2xl text-sky-500 text-center">
+        <Link href="/data">dynamicRout</Link>
+      </h1>
+
+      <motion.div className="w-[500px] flex flex-col gap-5 h-[600px] border-2 border-sky-500 rounded-lg text-white">
+        <motion.section
+          layout="position"
+          className="flex flex-col"
+          // initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          exit={{ opacity: 0, y: -10 }}
+        >
+          <div
+            className="px-5 flex justify-between text-2xl"
+            onClick={() => setShow(!show)}
+          >
+            <h1>Time</h1>
+            <h1> + </h1>
+          </div>
+          {show && (
+            <motion.div
+              className="px-5"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              exit={{ opacity: 0, y: -10 }}
+            >
+              <ul>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+              </ul>
+            </motion.div>
+          )}
+        </motion.section>
+
+        {/* second */}
+        <motion.section
+          layout="position"
+          className="flex flex-col"
+          // initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          exit={{ opacity: 0, y: -10 }}
+        >
+          <div
+            className="px-5 flex justify-between text-2xl"
+            onClick={() => setShow1(!show1)}
+          >
+            <h1>Time</h1>
+            <h1> + </h1>
+          </div>
+          {show1 && (
+            <motion.div
+              className="px-5"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              exit={{ opacity: 0, y: -10 }}
+            >
+              <ul>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+                <li>one</li>
+              </ul>
+            </motion.div>
+          )}
+        </motion.section>
+      </motion.div>
+    </main>
   );
 }
