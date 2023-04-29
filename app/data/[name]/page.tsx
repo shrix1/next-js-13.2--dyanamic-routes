6189b84f25ea76b37f360c51c07a8753c10c2c43
Shriprasanna <shriprasanna32@gmail.com>
2023-04-29 21:58:36 +0530

    issue fixed

--- app/data/[name]/page.tsx
+++ app/data/[name]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import data from "../../api/hello/data.json";
 import Link from "next/link";
 
@@ -13,7 +13,6 @@ export async function generateMetadata({ params }: paramsType) {
 }
 
 async function check() {
-  await new Promise((resolve) => setTimeout(resolve, 1500));
   return data.filter((v) => v.name === "cs");
 }
 
@@ -22,15 +21,17 @@ const dynamicRouteName = async ({ params }: paramsType) => {
 
   return (
     <>
-      <div className="text-white flex flex-col p-5 border border-sky-500 w-[350px]">
-        {params.name}
-        <h1>name of the game : {filterName[0]?.name} </h1>
-        <h1> rating out of 10 : {filterName[0]?.rating}</h1>
-      </div>
+      <Suspense fallback={<h1 className="text-white text-4xl">Loading</h1>}>
+        <div className="text-white flex flex-col p-5 border border-sky-500 w-[350px]">
+          {params.name}
+          <h1>name of the game : {filterName[0]?.name} </h1>
+          <h1> rating out of 10 : {filterName[0]?.rating}</h1>
+        </div>
 
-      <Link href="/data" className="bg-sky-500 p-2">
-        <button>Back to all games</button>
-      </Link>
+        <Link href="/data" className="bg-sky-500 p-2">
+          <button>Back to all games</button>
+        </Link>
+      </Suspense>
     </>
   );
 };
