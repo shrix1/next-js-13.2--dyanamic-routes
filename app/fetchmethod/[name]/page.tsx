1e7e9257a5b58cccaf73d353528e26f655067043
Shriprasanna <shriprasanna32@gmail.com>
2023-05-12 11:51:29 +0530

    calendar check

--- app/fetchmethod/[name]/page.tsx
+++ app/fetchmethod/[name]/page.tsx
@@ -1,31 +1,29 @@
-import React from "react"
+// import React from "react"
 
-interface gamesType {
-  name: string
-  rating: number
-  catorgory: string
-}
+// interface gamesType {
+//   name: string
+//   rating: number
+//   catorgory: string
+// }
 
-const Page = async ({ params }: any) => {
-  async function getGames() {
-    const data = await fetch(
-      "https://next-js-13-2-dyanamic-routes.vercel.app/api/game"
-    )
-    const res = await data.json()
-    return res.filter((v: gamesType) => v.name === params.name)
-  }
+// const Page = async ({ params }: any) => {
+//   async function getGames() {
+//     const data = await fetch("http://localhost:3000/api/game")
+//     const res = await data.json()
+//     return res.filter((v: gamesType) => v.name === params.name)
+//   }
 
-  const games = await getGames()
+//   const games = await getGames()
 
-  return (
-    <div className="flex text-xl gap-10">
-      <span> {games[0].name} </span>
-      <span>|</span>
-      <span> {games[0].rating}</span>
-      <span>|</span>
-      <span> {games[0].catorgory}</span>
-    </div>
-  )
-}
+//   return (
+//     <div className="flex text-xl gap-10">
+//       <span> {games[0].name} </span>
+//       <span>|</span>
+//       <span> {games[0].rating}</span>
+//       <span>|</span>
+//       <span> {games[0].catorgory}</span>
+//     </div>
+//   )
+// }
 
-export default Page
+// export default Page
--- app/fetchmethod/page.tsx
+++ app/fetchmethod/page.tsx
@@ -1,38 +1,36 @@
-import React from "react"
-import Link from "next/link"
+// import React from "react"
+// import Link from "next/link"
 
-interface gamesType {
-  name: string
-  rating: number
-  catorgory: string
-}
+// interface gamesType {
+//   name: string
+//   rating: number
+//   catorgory: string
+// }
 
-const Page = async () => {
-  async function getGames() {
-    const data = await fetch(
-      "https://next-js-13-2-dyanamic-routes.vercel.app/api/game"
-    )
-    const res = await data.json()
-    return res
-  }
+// const Page = async () => {
+//   async function getGames() {
+//     const data = await fetch("http://localhost:3000/api/game")
+//     const res = await data.json()
+//     return res
+//   }
 
-  const games: gamesType[] = await getGames()
+//   const games: gamesType[] = await getGames()
 
-  return (
-    <>
-      {games.map((info, idx: number) => {
-        return (
-          <Link
-            href={`/fetchmethod/${info.name}`}
-            key={idx}
-            className="border p-2 border-white/30 mb-1 rounded-md"
-          >
-            {info.name} | {info.rating} | {info.catorgory}
-          </Link>
-        )
-      })}
-    </>
-  )
-}
+//   return (
+//     <>
+//       {games.map((info, idx: number) => {
+//         return (
+//           <Link
+//             href={`/fetchmethod/${info.name}`}
+//             key={idx}
+//             className="border p-2 border-white/30 mb-1 rounded-md"
+//           >
+//             {info.name} | {info.rating} | {info.catorgory}
+//           </Link>
+//         )
+//       })}
+//     </>
+//   )
+// }
 
-export default Page
+// export default Page
