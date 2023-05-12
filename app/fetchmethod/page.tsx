cd720a60aa459f794bbf0fa975bcbce6d09f2c7c
Shriprasanna <shriprasanna32@gmail.com>
2023-05-12 11:53:38 +0530

    commented

--- app/fetchmethod/page.tsx
+++ app/fetchmethod/page.tsx
@@ -1,36 +1,37 @@
-// import React from "react"
-// import Link from "next/link"
+import React from "react"
+import Link from "next/link"
 
-// interface gamesType {
-//   name: string
-//   rating: number
-//   catorgory: string
-// }
+interface gamesType {
+  name: string
+  rating: number
+  catorgory: string
+}
 
-// const Page = async () => {
-//   async function getGames() {
-//     const data = await fetch("http://localhost:3000/api/game")
-//     const res = await data.json()
-//     return res
-//   }
+const Page = async () => {
+  // async function getGames() {
+  //   const data = await fetch("http://localhost:3000/api/game")
+  //   const res = await data.json()
+  //   return res
+  // }
 
-//   const games: gamesType[] = await getGames()
+  // const games: gamesType[] = await getGames()
 
-//   return (
-//     <>
-//       {games.map((info, idx: number) => {
-//         return (
-//           <Link
-//             href={`/fetchmethod/${info.name}`}
-//             key={idx}
-//             className="border p-2 border-white/30 mb-1 rounded-md"
-//           >
-//             {info.name} | {info.rating} | {info.catorgory}
-//           </Link>
-//         )
-//       })}
-//     </>
-//   )
-// }
+  return (
+    <>
+      {/* {games.map((info, idx: number) => {
+        return (
+          <Link
+            href={`/fetchmethod/${info.name}`}
+            key={idx}
+            className="border p-2 border-white/30 mb-1 rounded-md"
+          >
+            {info.name} | {info.rating} | {info.catorgory}
+          </Link>
+        )
+      })} */}
+      Check
+    </>
+  )
+}
 
-// export default Page
+export default Page
